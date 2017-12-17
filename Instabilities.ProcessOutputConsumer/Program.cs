c473f3ab7f9d32980f61d143531cf467e6669146
Yan Drugalya <ydrugalya@gmail.com>
2017-12-17 14:19:08 +0100

    Process output read instability draft

---
+++ Instabilities.ProcessOutputConsumer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Instabilities.ProcessOutputConsumer
+{
+
+    public class Program
+    {
+        static void Main()
+        {
+            ProduceAndConsumeMessagesUsingQueue(1000);
+            ProduceAndConsumeMessagesUsingQueue(2000);
+            ProduceAndConsumeMessagesUsingQueue(3000);
+        }
+
+        static void ProduceAndConsumeMessagesUsingQueue(int expectedLinesCount)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "Instabilities.ProcessOutputProducer.exe",
+                Arguments = $"{expectedLinesCount}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+
+            var p = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            var processOutout = new Queue<string>();
+            p.OutputDataReceived += (sender, eventArgs) =>
+            {
+                if (!string.IsNullOrEmpty(eventArgs.Data))
+                {
+                    processOutout.Enqueue(eventArgs.Data);
+                }
+            };
+
+            p.Start();
+            p.BeginOutputReadLine();
+
+            var done = false;
+            var actualMessageCount = 0;
+            while (!done)
+            {
+                string msg;
+                while (processOutout.Count > 0  && (msg = processOutout.Dequeue()) != null)
+                {
+                    if (msg == "Done")
+                    {
+                        done = true;
+                        break;
+                    }
+                    ++actualMessageCount;
+                }
+                Thread.Sleep(10);
+            }
+
+            p.WaitForExit();
+            p.Close();
+            Console.WriteLine($"Lines read: {actualMessageCount} Expected: {expectedLinesCount}");
+        }
+
+    }
+}
---
+++ Instabilities.ProcessOutputProducer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Instabilities.ProcessOutputProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = int.Parse(args[0]);
+            
+            for (var i = 0; i < count; ++i)
+            {
+                Console.WriteLine($"Message #{i}");
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
